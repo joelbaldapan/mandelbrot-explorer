d0fb866bc52d237d6a14f4a26e0c0392f7965885
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-08-02 05:26:48 +0800

    feat: add mandelbrot set drawing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function loadShaderAsync(shaderURL, callback) {
-  var req = new XMLHttpRequest();
+  const req = new XMLHttpRequest();
   req.open("GET", shaderURL, true);
   req.onload = function () {
     if (req.status < 200 || req.status >= 300) {
@@ -22,14 +22,31 @@ function Init() {
   );
 }
 
-function RunDemo(loadErrors, loadedShaders) {
-  // Attach callbacks
-  AddEvent(window, "resize", OnResizeWindow);
-  AddEvent(window, "wheel", OnZoom);
-  AddEvent(window, "mousemove", OnMouseMove);
+function AddEvent(object, type, callback) {
+  if (object == null || typeof object == "undefined") return;
+  if (object.addEventListener) {
+    object.addEventListener(type, callback, false);
+  } else if (object.attachEvent) {
+    object.attachEvent("on" + type, callback);
+  } else {
+    object["on" + type] = callback;
+  }
+}
 
-  var canvas = document.getElementById("gl-surface");
-  var gl = canvas.getContext("webgl");
+function RemoveEvent(object, type, callback) {
+  if (object == null || typeof object == "undefined") return;
+  if (object.removeEventListener) {
+    object.removeEventListener(type, callback, false);
+  } else if (object.detachEvent) {
+    object.detachEvent("on" + type, callback);
+  } else {
+    object["on" + type] = callback;
+  }
+}
+
+function RunDemo(loadErrors, loadedShaders) {
+  const canvas = document.getElementById("gl-surface");
+  const gl = canvas.getContext("webgl");
   if (!gl) {
     console.log("Webgl context not available - falling back on experimental");
     gl = canvas.getContext("experimental-webgl");
@@ -38,4 +55,91 @@ function RunDemo(loadErrors, loadedShaders) {
     alert("Cannot get WebGL context - browser does not support WebGL");
     return;
   }
+
+  // Create shader program
+  const vs = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vs, loadedShaders.vsText);
+  gl.compileShader(vs);
+  if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+    console.error("Vertex shader compile error:", gl.getShaderInfoLog(vs));
+  }
+
+  const fs = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fs, loadedShaders.fsText);
+  gl.compileShader(fs);
+  if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+    console.error("Fragment shader compile error:", gl.getShaderInfoLog(fs));
+  }
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vs);
+  gl.attachShader(program, fs);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error("Shader program link error:", gl.getShaderInfoLog(program));
+  }
+
+  gl.validateProgram(program);
+  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+    console.error(
+      "Shader program validate error:",
+      gl.getShaderInfoLog(program)
+    );
+  }
+
+  gl.useProgram(program);
+
+  // Get uniform locations
+  let uniforms = {
+    viewportDimensions: gl.getUniformLocation(program, "viewportDimensions"),
+    minI: gl.getUniformLocation(program, "minI"),
+    maxI: gl.getUniformLocation(program, "maxI"),
+    minR: gl.getUniformLocation(program, "minR"),
+    maxR: gl.getUniformLocation(program, "maxR"),
+  };
+
+  // Set CPU-side variables for all of our shader variables
+  const vpDimensions = [canvas.clientWidth, canvas.clientHeight];
+  const minI = -2.0;
+  const maxI = 2.0;
+  const minR = -2.0;
+  const maxR = 2.0;
+
+  // Create buffers
+  const vertexBuffer = gl.createBuffer();
+  const vertices = [
+    -1, 1, -1, -1, 1, -1,
+
+    -1, 1, 1, 1, 1, -1,
+  ];
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  const vPosAttrib = gl.getAttribLocation(program, "vPos");
+  gl.vertexAttribPointer(
+    vPosAttrib,
+    2,
+    gl.FLOAT,
+    gl.FALSE,
+    2 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.enableVertexAttribArray(vPosAttrib);
+
+  const loop = function () {
+    // Draw
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+    gl.uniform2fv(uniforms.viewportDimensions, vpDimensions);
+    gl.uniform1f(uniforms.minI, minI);
+    gl.uniform1f(uniforms.maxI, maxI);
+    gl.uniform1f(uniforms.minR, minR);
+    gl.uniform1f(uniforms.maxR, maxR);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    requestAnimationFrame(loop);
+  };
+  requestAnimationFrame(loop);
 }
---
+++ mandl.fs.glsl
@@ -0,0 +1,70 @@
+precision highp float;
+
+uniform vec2 viewportDimensions;
+uniform float minI;
+uniform float maxI;
+uniform float minR;
+uniform float maxR;
+
+vec3 hsbToRgb(float h, float s, float b) {
+    h = mod(h, 1.0) * 6.0;
+    float c = b * s;
+    float x = c * (1.0 - abs(mod(h, 2.0) - 1.0));
+    vec3 rgb;
+    if (0.0 <= h && h < 1.0) {
+        rgb = vec3(c, x, 0.0);
+    } else if (1.0 <= h && h < 2.0) {
+        rgb = vec3(x, c, 0.0);
+    } else if (2.0 <= h && h < 3.0) {
+        rgb = vec3(0.0, c, x);
+    } else if (3.0 <= h && h < 4.0) {
+        rgb = vec3(0.0, x, c);
+    } else if (4.0 <= h && h < 5.0) {
+        rgb = vec3(x, 0.0, c);
+    } else {
+        rgb = vec3(c, 0.0, x);
+    }
+    return rgb + b - c;
+}
+
+void main() {
+    // Adjust coordinates with respect to canvas width and height
+    vec2 c = vec2(
+        gl_FragCoord.x * (maxR - minR) / viewportDimensions.x + minR,
+        gl_FragCoord.y * (maxI - minI) / viewportDimensions.y + minI
+    );
+
+    // Mandelbrot formula:
+    vec2 z = c;
+    float iterations = 0.0;
+    float maxIterations = 20000.0;
+    const int imaxIterations = 20000;
+    bool escaped = false;
+
+    for (int i = 0; i < imaxIterations; i++) {
+        if (!escaped) {
+            float t = 2.0 * z.x * z.y + c.y;
+            z.x = z.x * z.x - z.y * z.y + c.x;
+            z.y = t;
+
+            if (z.x * z.x + z.y * z.y > 4.0) {
+                escaped = true;
+                iterations = float(i);
+            }
+        }
+    }
+
+    if (iterations < maxIterations) {
+        // Color calculation
+        float hue = 200.0 + mod(sqrt(iterations / 50.0) * 1.0, 255.0);
+        float sat = 80.0 / 100.0;
+        float bri = 10.0 + sqrt(iterations / 50.0) * 80.0;
+        bri = bri / 100.0;
+
+        vec3 color = hsbToRgb(hue / 360.0, sat, bri);
+        gl_FragColor = vec4(color, 1.0);
+    } else {
+        // Default color for points inside the Mandelbrot set
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+}
